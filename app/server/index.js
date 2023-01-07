eab8ce9f2c1694926b4d515f45a4271ec7239257
AlexGaillard <alex@radish.health>
2023-01-07 15:23:36 -0500

    Change server folder structure

--- app/server/index.js
+++ app/server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
-const bot = require("./bot");
+const bot = require("../bot");
 
 const app = express();
 
